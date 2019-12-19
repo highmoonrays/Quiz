162d2821cba28e24cf75075599c483463d51fed7
highmoonrays <you@example.com>
2019-12-19 20:46:48 +0300

    trying to make reset password using email senders

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,18 @@
 declare(strict_types=1);
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Mailer\SentMessage;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +40,43 @@ class SecurityController extends AbstractController
     public function logout()
     {
         return $this->redirectToRoute('app_login');
+    }
+
+    /**
+     * @Route("/reset-password", name="reset_password", methods={"GET", "POST"})
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function resetPassword(Request $request, UserRepository $userRepository, \Swift_Mailer $mailer,
+                                  LoggerInterface $logger): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            if ($this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $form['email']->getData()])) {
+//                $name = $request->query->get('name');
+//                $message = new \Swift_Message('Test email');
+//                $message->setFrom('admin@zetcode.com');
+//                $message->setTo('admin2@zetcode.com');
+//                $message->setBody(
+//                    $this->renderView(
+//                        'emails/mymail.html.twig',
+//                        ['name' => $name]
+//                    ),
+//                    'text/html'
+//                );
+//                $mailer->send($message);
+//                $logger->info('email sent');
+//                $this->addFlash('notice', 'Email sent');
+                $this->addFlash('success', 'Message has been sent to your email pal');
+            } else {
+                $this->addFlash('wrong', 'Did you wrote correct email?');
+            }
+        }
+        return $this->render('security/reset_password.html.twig',[
+                'form' => $form->createView(),
+            ]);
     }
 }
